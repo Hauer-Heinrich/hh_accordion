001189fcdb6a7b7720f1f72e847406015572b31f
Christian Hackl <hackl.chris@googlemail.com>
2023-01-16 13:15:56 +0100

    [BUGFIX] PHP 8 undefined array key

--- Classes/Form/FormDataProvider/TcaColPosItem.php
+++ Classes/Form/FormDataProvider/TcaColPosItem.php
@@ -1,7 +1,8 @@
 <?php
 namespace HauerHeinrich\HhAccordion\Form\FormDataProvider;
 
-use TYPO3\CMS\Backend\Form\FormDataProviderInterface;
+// use \TYPO3\CMS\Extbase\Utility\DebuggerUtility;
+use \TYPO3\CMS\Backend\Form\FormDataProviderInterface;
 
 class TcaColPosItem implements FormDataProviderInterface {
     /**
@@ -27,18 +28,20 @@ class TcaColPosItem implements FormDataProviderInterface {
             return $result;
         }
 
-        if (!is_array($result['processedTca']['columns']['colPos']['config']['items'])) {
-            $result['processedTca']['columns']['colPos']['config']['items'] = [];
-        }
+        if (isset($result['processedTca']['columns']['colPos']['config']['items'])) {
+            if (!is_array($result['processedTca']['columns']['colPos']['config']['items'])) {
+                $result['processedTca']['columns']['colPos']['config']['items'] = [];
+            }
 
-        array_unshift(
-            $result['processedTca']['columns']['colPos']['config']['items'],
-            [
-                'LLL:EXT:hh_accordion/Resources/Private/Language/locallang_db.xlf:tt_content.colPos.nestedContentColPos',
-                $result['databaseRow']['colPos'],
-            ]
-        );
-        unset($result['processedTca']['columns']['colPos']['config']['itemsProcFunc']);
+            array_unshift(
+                $result['processedTca']['columns']['colPos']['config']['items'],
+                [
+                    'LLL:EXT:hh_accordion/Resources/Private/Language/locallang_db.xlf:tt_content.colPos.nestedContentColPos',
+                    $result['databaseRow']['colPos'],
+                ]
+            );
+            unset($result['processedTca']['columns']['colPos']['config']['itemsProcFunc']);
+        }
 
         return $result;
     }
