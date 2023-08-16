403223a0c858bdbcd81724ef9d0b3225b9a074c7
Christian Hackl <hackl.chris@googlemail.com>
2023-08-16 11:46:03 +0200

    [TASK] set version to 0.5.1

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "hh_accordion".
  *
- * Auto generated 08-08-2023 12:10
+ * Auto generated 16-08-2023 10:41
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -18,7 +18,7 @@ $EM_CONF['hh_accordion'] = [
     'author_email' => 'chackl@hauer-heinrich.de',
     'author_company' => 'www.hauer-heinrich.de',
     'state' => 'stable',
-    'version' => '0.5.0',
+    'version' => '0.5.1',
     'constraints' => [
         'depends' => [
             'typo3' => '11.5.0-12.4.99',
